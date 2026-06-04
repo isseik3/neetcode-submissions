b3064afee5f29c57692cc7d4f12b1a65bdede353
Issei Kimura <11478101+isseik3@users.noreply.github.com>
2026-06-04 16:02:18 +0900

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.go
@@ -0,0 +1,17 @@
+func lengthOfLongestSubstring(s string) int {
+	// charSet stores the index right after each character's last occurrence
+	charSet := make(map[byte]int, len(s))
+	left, best := 0, 0
+	for i := 0; i < len(s); i++ {
+		if idx, ok := charSet[s[i]]; ok && left < idx {
+			left = idx
+		}
+		diff := i - left + 1
+		if diff > best {
+			best = diff
+		}
+		charSet[s[i]] = i + 1
+	}
+	return best
+
+}
