70f1683af8675253afbaa0c70bef021ec149ab14
Issei Kimura <11478101+isseik3@users.noreply.github.com>
2026-06-05 13:45:08 +0900

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.go
@@ -0,0 +1,30 @@
+type MinStack struct {
+	stack []int
+	min []int
+}
+
+func Constructor() MinStack {
+	return MinStack{}
+}
+
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+	if len(this.min) == 0 || this.min[len(this.min)-1] > val {
+		this.min = append(this.min, val)
+	} else {
+		this.min = append(this.min, this.min[len(this.min)-1])
+	}
+}
+
+func (this *MinStack) Pop() {
+	this.stack = this.stack[:len(this.stack)-1]
+	this.min = this.min[:len(this.min)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.min[len(this.min)-1]
+}
