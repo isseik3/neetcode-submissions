17f3754fe1a3eed3785502ded41f3fff833090aa
Issei Kimura <11478101+isseik3@users.noreply.github.com>
2026-06-05 19:13:20 +0900

    Add: find-target-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-2.go
@@ -0,0 +1,24 @@
+func search(nums []int, target int) int {
+	left, right := 0, len(nums) - 1
+	for left <= right {
+		mid := left + (right - left) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		// determine which half is sorted
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid
+			}
+		}
+	}
+	return -1
+}
