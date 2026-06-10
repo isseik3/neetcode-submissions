cb01379c807f925cee22396c4f5b2c359be65315
Issei Kimura <11478101+isseik3@users.noreply.github.com>
2026-06-10 17:39:12 +0900

    Add: binary-tree-diameter - submission-1

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-1.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func diameterOfBinaryTree(root *TreeNode) int {
+	best := 0
+	var height func(node *TreeNode) int
+	height = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		left := height(node.Left)
+		right := height(node.Right)
+		if best < left + right {
+			best = left + right
+		}
+		if left > right {
+			return left + 1
+		} else {
+			return right + 1
+		}
+	}
+	height(root)
+	return best 
+}
